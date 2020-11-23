a79b9ae479c07c9aeed8fc0fe3c3567547c2c0ff
RichardVN <59857903+RichardVN@users.noreply.github.com>
2020-11-22 16:29:19 -0800

    add/delete/update for houses

--- app.py
+++ app.py
@@ -66,7 +66,8 @@ class Professors(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255), nullable=False)
     last_name = db.Column(db.String(255), nullable=False)
-    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'))
+    house_id = db.Column(db.Integer, db.ForeignKey(
+        'houses.id', ondelete='SET NULL'))
 
 
 class Students(db.Model):
@@ -75,22 +76,24 @@ class Students(db.Model):
     last_name = db.Column(db.String(255), nullable=False)
     birthdate = db.Column(db.Date, nullable=False)
     year = db.Column(db.Integer, nullable=False)
-    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'))
+    house_id = db.Column(db.Integer, db.ForeignKey(
+        'houses.id', ondelete='SET NULL'))
 
 
 class Classes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     credit = db.Column(db.String(255), nullable=False)
-    professor_id = db.Column(db.Integer, db.ForeignKey('professors.id'))
+    professor_id = db.Column(db.Integer, db.ForeignKey(
+        'professors.id', ondelete='SET NULL'))
 
 
 # Many to Many relationship table
 registration_table = db.Table('StudentsClasses', db.Model.metadata,
                               db.Column('student_id', db.Integer,
-                                        db.ForeignKey('students.id')),
+                                        db.ForeignKey('students.id', ondelete='CASCADE')),
                               db.Column('class_id', db.Integer,
-                                        db.ForeignKey('classes.id'))
+                                        db.ForeignKey('classes.id', ondelete='CASCADE'))
                               )
 
 # route decorator tells Flask what URL should trigger our function. Just calling a website with url is a GET request.
@@ -126,7 +129,7 @@ def show_students():
         student_houses = [Houses.query.filter_by(
             id=student.house_id).first() for student in students]
         for idx, house in enumerate(student_houses):
-            student_houses[idx] = house.name
+            student_houses[idx] = house.name if house else None
 
         return render_template('students.html', students=students, houses=houses, student_houses=student_houses)
     elif request.method == 'POST':
@@ -198,8 +201,70 @@ def update_student(id):
 
 @app.route('/houses', methods=['POST', 'GET'])
 def show_houses():
-    return render_template('houses.html')
+    if request.method == "GET":
+        houses = Houses.query.order_by(Houses.name).all()
+        return render_template('houses.html', houses=houses,)
+    elif request.method == "POST":
+        if request.form['post_type'] == "add":
+            print("\n------- index POST add -----")
+            print("Adding a house to the database")
 
+            name = request.form['name']
+            mascot = request.form['mascot']
+            founder = request.form['founder']
+            head = request.form['head']
+
+            new_house = Houses(
+                name=name, mascot=mascot, founder=founder, head=head)
+
+            try:
+                db.session.add(new_house)
+                db.session.commit()
+                print(
+                    f'POST: new house: {name} successfully added')
+            except:
+                return("There was an error adding that house.")
+
+            return redirect('/houses')
+
+        # delete 'post_type' == 'delete'
+        else:
+            print("\n------- index POST delete -----")
+            print("deleting a house to the database")
+            delete_id = request.form['delete_id']
+            house_to_delete = Houses.query.get_or_404(delete_id)
+            try:
+                print(
+                    f"Deleting House ID:{house_to_delete.id}, {house_to_delete.name} {house_to_delete.mascot}. ", end="")
+                db.session.delete(house_to_delete)
+                db.session.commit()
+                print("Deletion Successful!")
+            except:
+                return("There was an error deleting that house.")
+
+            return redirect('/houses')
+
+
+@app.route('/update-house/<int:id>', methods=['POST', 'GET'])
+def update_house(id):
+    if request.method == 'GET':
+        house = Houses.query.get_or_404(id)
+        return render_template('update_houses.html', house=house)
+    elif request.method == 'POST':
+        # update values
+        house_to_update = Houses.query.get_or_404(id)
+        print(
+            f"updating values for: {house_to_update.id} {house_to_update.name} {house_to_update.mascot}")
+        house_to_update.name = request.form['name']
+        house_to_update.mascot = request.form['mascot']
+        house_to_update.founder = request.form['founder']
+        house_to_update.head = request.form['head']
+        try:
+            db.session.commit()
+            print("Update Successful!")
+        except:
+            return "There was an error updating the house"
+        return redirect('/houses')
 
 @app.route('/professors', methods=['POST', 'GET'])
 def show_professors():
